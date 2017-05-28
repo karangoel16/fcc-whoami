515134921ef6d365ba3349d3a798611dab0c7611
karangoel16 <karangoel16@yahoo.com>
2017-05-28 19:36:32 +0530

    Some error in ip

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 app.get('/',function(req,res,next){
-	var info={"ipaddress":req.ip,"language":req.headers["accept-language"].split(",")[0],"software":req.headers["user-agent"]};
+	var info={"ipaddress":req.ip.split(':')[3],"language":req.headers["accept-language"].split(",")[0],"software":req.headers["user-agent"]};
 	console.log(req.connection.remoteAddress);
 	res.send(info);
 });
