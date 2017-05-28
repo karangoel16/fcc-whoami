8571e2c712f1c13717c3a9240e12a080e929c8b8
karangoel16 <karangoel16@yahoo.com>
2017-05-28 19:44:17 +0530

    User agent rectified

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var useragent = require('useragent');
 var app = express();
+useragent(true);
 var port = process.env.PORT || 3000;
 app.get('/',function(req,res,next){
-	var info={"ipaddress":req.ip.split(':')[3],"language":req.headers["accept-language"].split(",")[0],"software":req.headers["user-agent"]};
-	console.log(req.connection.remoteAddress);
+	var agent=useragent.parse(req.headers['user-agent']);
+	var info={"ipaddress":req.ip.split(':')[3],"language":req.headers["accept-language"].split(",")[0],"software":agent.os.toString()};
+	//console.log(req.connection.remoteAddress);
 	res.send(info);
 });
 app.listen(port,function()
